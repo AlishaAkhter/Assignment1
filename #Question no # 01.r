4a728f11ac4b1768ea358f3749a5a0aed5d24c12
AlishaAkhter <qabilbano374@gmail.com>
2024-11-02 04:12:34 -0700

    Add files via upload

---
+++ #Question no # 01.r
@@ -0,0 +1,33 @@
+
+
+number1=10
+number2=15
+number3=20
+sum=number1+number2+number3
+print(sum) #sum 
+difference=number1-number2-number3
+print(difference) #difference
+product=number1*number2*number3
+print(product) #product
+average=number1+number2+number3\3
+print(average) #average
+breadth=5
+height=5
+area=0.5*breadth*height
+print(area) #area of triangle
+side=3
+side=3
+area=side*side
+print(area) #area of square
+radius=5
+area=3.142*radius*radius
+print(area) #area of circle
+length=3
+width=3
+area=length*width
+print(area) #area of rectangle
+
+
+
+
+
